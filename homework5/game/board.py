b20c463491ce45f6ff491733da23a2985c1e3dfc
Milka Ferezliyska <mileto94@abv.bg>
2016-11-27 20:41:36 +0200

    Add pre release

--- homework5/game/board.py
+++ homework5/game/board.py
@@ -1,10 +1,6 @@
-import json
-from random import randint
-from copy import deepcopy
-from heapq import heappush, nlargest
+from random import randint, choice
 
-
-INFINITY = 2
+INFINITY = 100
 
 
 class Board:
@@ -13,12 +9,14 @@ class Board:
         self.size = 3
         self.player = 'X'
         self.opponent = 'O'
-        self.turn = 0
         self.fields = {(i, j): self.empty for j in range(self.size) for i in range(self.size)}
-        self.comp_sign = 'X' if is_computer else 'O'
-        self.moves = []
 
-        self.next_moves = [pos for pos, sign in self.fields.items() if sign == self.empty]
+        self.comp_sign = 'X' if is_computer else 'O'
+
+        self.win_states = [[(i, j) for j in range(3)] for i in range(3)]  # col
+        self.win_states.extend([[(j, i) for j in range(3)] for i in range(3)])  # row
+        self.win_states.append([(i, i) for i in range(self.size)])  # diag
+        self.win_states.append([(0, 2), (1, 1), (2, 0)])  # rev_diag
 
     def __str__(self):
         board = []
@@ -29,95 +27,98 @@ class Board:
             board.append('|'.join(row))
         return '\n'.join(board)
 
-    def is_winner(self):
-        if self.turn == 0: return False, None
-        for i in range(self.size):
-            row, col, diag, rev_diag = [], [], [], []
-            for pos, sign in self.fields.items():
-                if pos in self.next_moves: continue
-                if pos[0] == i:
-                    row.append(sign)
-                if pos[1] == i:
-                    col.append(sign)
-                if pos == (i, i):
-                    diag.append(sign)
-                if pos[0] + pos[1] == 2:
-                    rev_diag.append(sign)
-            if row and len(row) == self.size and len(set(row)) == 1:
-                print('row', row)
-                return True, row[0]
-            if col and len(col) == self.size and len(set(col)) == 1:
-                print('col', col)
-                return True, col[0]
-            if diag and len(diag) == self.size and len(set(diag)) == 1:
-                print('diag', diag)
-                return True, diag[0]
-            if rev_diag and len(rev_diag) == self.size and len(set(rev_diag)) == 1:
-                print('rev_diag', rev_diag)
-                return True, rev_diag[0]
-        return False, None
+    def get_available_moves(self):
+        return [pos for pos, sign in self.fields.items() if sign == self.empty]
 
-    def wins(self):
-        diag, rev_diag = [], []
-        for i in range(self.size):
-            row, col = [], []
-            for pos, sign in self.fields.items():
-                if sign != self.empty and pos[0] == i:
-                    row.append(sign)
-                if sign != self.empty and pos[1] == i:
-                    col.append(sign)
-                if sign != self.empty and pos == (i, i):
-                    diag.append(sign)
-                if sign != self.empty and pos[0] + pos[1] == 2:
-                    rev_diag.append(sign)
+    def get_fields(self, player):
+        return [pos for pos, sign in self.fields.items() if sign == player]
 
-                # print(row)
-                # print(col)
-                # print(diag)
-                # print(rev_diag)
-                if len(row) == 3 and len(set(row)) == 1:
-                    if sign == self.comp_sign:
-                        return 1
-                    return -1
+    def is_game_over(self):
+        if self.get_available_moves() == 0 or self.get_winner() is not None:
+            return True
+        return False
 
-                if len(col) == 3 and len(set(col)) == 1:
-                    if sign == self.comp_sign:
-                        return 1
-                    return -1
+    def min_max(self, is_max, alpha, beta):
+        if self.is_game_over():
+            wins = self.get_winner()
+            if wins == self.comp_sign: return 1
+            elif wins: return -1
+            else: return 0
 
-                if len(diag) == 3 and len(set(diag)) == 1:
-                    if sign == self.comp_sign:
-                        return 1
-                    return -1
+        for pos in self.get_available_moves():
+            self.fields[pos] = self.player
+            val = self.min_max(not is_max, alpha, beta)
+            self.fields[pos] = self.empty
+            if is_max:
+                if val > alpha:
+                    alpha = val
+                if alpha >= beta:
+                    return beta
 
-                if len(rev_diag) == 3 and len(set(rev_diag)) == 1:
-                    if sign == self.comp_sign:
-                        return 1
-                    return -1
-        return 0
+            else:
+                if val < beta:
+                    beta = val
+                if beta <= alpha:
+                    return alpha
+
+        return alpha if is_max else beta
+
+    def get_best(self, is_max):
+        final_res = -INFINITY
+        winners = ('X-win', 'Draw', 'O-win')
+
+        positions = self.get_available_moves()
+        if len(positions) == self.size ** 2:
+            return 1, 1
+
+        choices = []
+        for pos in positions:
+            self.fields[pos] = self.player
+            val = self.min_max(not is_max, -INFINITY, INFINITY)
+            self.fields[pos] = self.empty
+            print("move:", pos, "causes:", winners[val + 1])
+            if val > final_res:
+                final_res = val
+                choices = [pos]
+            elif val == final_res:
+                choices.append(pos)
+        print('Choices', choices)
+        return choice(choices)
+
+    # def get_winner(self):
+    #     for player in [self.player, self.opponent]:
+    #         positions = self.get_fields(player)
+
+    #         if not positions: continue
+    #         for win_state in self.win_states:
+    #             if set(positions).intersection(set(win_state)) == self.size:
+    #                 return player
+
+    #     return None
+
+    def get_winner(self):
+        for player in [self.player, self.opponent]:
+            positions = self.get_fields(player)
+            if not positions: continue
+
+            for win_state in self.win_states:
+                win = True
+                for pos in win_state:
+                    if pos not in positions:
+                        win = False
+                if win:
+                    return player
+
+        return None
 
     def move(self, row, col):
-        # if row in range(1, 4) and col in range(1, 4) and self.fields[row - 1, col - 1] == self.empty:
-        if (row, col) in self.next_moves:
-            # self.fields[row - 1, col - 1] = self.player
+        if (row, col) in self.get_available_moves():
             self.fields[row, col] = self.player
             self.player, self.opponent = self.opponent, self.player
-            self.turn += 1
-            # self.next_moves.remove((row - 1, col - 1))
-            self.next_moves.remove((row, col))
             return True
         else:
-            print("MOVE ILLEGAL!!!")
-            1/0
             print('Illegal move! Try again...')
             return False
-
-    def update_fields(self, values):
-        self.fields = values
-        self.player, self.opponent = self.opponent, self.player
-        self.turn += 1
-        self.next_moves = [pos for pos, sign in self.fields.items() if sign == self.empty]
-        self.moves = []
 
     def check_user_input(self, row, col):
         if row in range(1, 4) and col in range(1, 4) and self.fields[row - 1, col - 1] == self.empty:
@@ -125,111 +126,13 @@ class Board:
         print('Illegal move! Try again...')
         return False
 
-    """
-    Pseudo code:
-
-    function minimax(node, depth, isMaximizingPlayer, alpha, beta):
-        if node is a leaf node: // i.e. depth == 3
-            return value of the node
-
-        if isMaximizingPlayer:
-            bestVal = -INFINITY
-            for each child node:
-                value = minimax(node, depth + 1, false, alpha, beta)
-                bestVal = max(bestVal, value)
-                alpha = max(alpha, bestVal)
-                if beta <= alpha:
-                    break
-            return bestVal
-
-        else:
-            bestVal = +INFINITY
-            for each child node:
-                value = minimax(node, depth + 1, true, alpha, beta)
-                bestVal = min(bestVal, value)
-                beta = min(beta, bestVal)
-                if beta <= alpha:
-                    break
-            return bestVal
-    """
-
-    def minimax(self, depth, is_max_player, alpha, beta):
-        if depth == self.size:
-            # print('DEPTH recursion')
-            # print(self)
-            # print()
-            return self.wins()
-
-        if is_max_player:
-            # print('MAX turn')
-            # best_value = -INFINITY
-            for child in self.get_moves():
-                # print_board(child)
-                value = child.minimax(depth + 1, False, alpha, beta)
-                if (value, str(child)) in self.moves:
-                    self.moves.remove((value, str(child)))
-                heappush(self.moves, (value, str(child)))
-                # best_value = max(best_value, value)
-                alpha = max(alpha, value)
-                if beta <= alpha:
-                    # print('(* β cut-off *)')
-                    break
-            return alpha
-        else:
-            # print('min turn')
-            # best_value = INFINITY
-            for child in self.get_moves():
-                # print_board(child)
-                value = child.minimax(depth + 1, True, alpha, beta)
-                # best_value = min(best_value, value)
-                if (value, str(child)) in self.moves:
-                    self.moves.remove((value, str(child)))
-                heappush(self.moves, (value, str(child)))
-                beta = min(beta, value)
-                if beta <= alpha:
-                    # print('(* α cut-off *)')
-                    break
-            return beta
-
-    def get_moves(self):
-        res = []
-        for pos in self.next_moves:
-            new_board = deepcopy(self)
-            new_board.move(*pos)
-            res.append(new_board)
-        return res
-
-    def get_best_move(self):
-        score, best_move = nlargest(1, self.moves)[0]
-        fields = {}
-        for i, row in enumerate(best_move.split('\n')):
-            items = row.split('|')
-            for j, item in enumerate(items):
-                fields[i, j] = item
-        print(fields)
-        return fields
-
-    def play_random(self):
-        row, col = self.next_moves[randint(0, len(self.next_moves)) - 1]
-        # row, col = randint(1, 3), randint(1, 3)
-        print(row + 1, col + 1)
-        print("PLAY RANDOM")
-        self.move(row, col)
-
     def play(self, is_computer):
-        who = None
-        while True:
+        while not self.is_game_over():
+
             if is_computer:
-                if self.turn == 0:
-                    self.play_random()
-                else:
-                    self.minimax(0, is_computer, -INFINITY, INFINITY)
-                    values = self.get_best_move()
-                    self.update_fields(values)
-                    print('THE BEST MOVE', values)
-                    # TODO: Play the best option
-                    # if not val:
-                    #     self.play_random()
+                best_position = self.get_best(is_computer)
+                print('THE BEST MOVE', best_position)
+                self.move(*best_position)
                 is_computer = False
 
             else:
@@ -240,8 +143,12 @@ class Board:
                     is_moved = self.check_user_input(row, col)
                 self.move(row - 1, col - 1)
                 is_computer = True
+
             print(self)
             print()
-            if self.wins(): break
-        print('WINNER: {}'.format(who))
 
+        winner = self.get_winner()
+        if winner:
+            print('WINNER: {}'.format(winner))
+        else:
+            print('It\'s a tie...')
--- homework5/game/game.py
+++ homework5/game/game.py
@@ -20,32 +20,5 @@ def main():
     print(board)
 
 
-def test_winner():
-    board = Board(True)
-    # Check rows
-    # board.fields.update({(0, 0): 'X', (0, 1): 'X', (0, 2): 'X'})
-    # board.fields.update({(1, 0): 'X', (1, 1): 'X', (1, 2): 'X'})
-    # board.fields.update({(2, 0): 'X', (2, 1): 'X', (2, 2): 'X'})
-
-    # Check cols
-    # board.fields.update({(0, 0): 'X', (1, 0): 'X', (2, 0): 'X'})
-    # board.fields.update({(0, 1): 'X', (1, 1): 'X', (2, 1): 'X'})
-    # board.fields.update({(0, 2): 'X', (1, 2): 'X', (2, 2): 'X'})
-
-    # Check diag
-    # board.fields.update({(0, 0): 'X', (1, 1): 'X', (2, 2): 'X'})
-
-    # Check rev_diag
-    board.fields.update({(0, 2): 'X', (1, 1): 'X', (2, 0): 'X'})
-    # board.fields.update({(0, 2): 'O', (1, 1): 'O', (2, 0): 'O'})
-
-    print(board)
-    wins = board.wins()
-    print(wins)
-    # wins, who = board.is_winner()
-    assert wins == 1, 'Oops, NOT found a winner!'
-    # assert who == 'X', 'Wrong sign for winner'
-
-
 if __name__ == '__main__':
     main()
