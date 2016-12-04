e63b016cc654cf6b0d41243976e6f1165bb5f971
Milka Ferezliyska <mileto94@abv.bg>
2016-12-04 13:18:13 +0200

    Add data file.

--- homework6/kNN.py
+++ homework6/kNN.py
@@ -1,6 +1,7 @@
 from heapq import heappush, nlargest, heappop
 from math import sqrt
 
+
 def update_class(bucket, name):
     if name in bucket:
         bucket[name] += 1
@@ -33,34 +34,35 @@ def generate_queue(item, elements, k):
 
 def main():
     dataset = [
-        [5.1,3.5,1.4,0.2,'Iris-setosa'],
-        [4.9,3.0,1.4,0.2,'Iris-setosa'],
-        [4.7,3.2,1.3,0.2,'Iris-setosa'],
-        [4.6,3.1,1.5,0.2,'Iris-setosa'],
-        [5.0,3.3,1.4,0.2,'Iris-setosa'],
-        [6.0,2.2,4.0,1.0,'Iris-versicolor'],
-        # [6.1,2.9,4.7,1.4,'Iris-versicolor'],
-        [5.4,3.0,4.5,1.5,'Iris-versicolor'],
-        [6.0,3.4,4.5,1.6,'Iris-versicolor'],
-        [6.7,3.1,4.7,1.5,'Iris-versicolor'],
-        [5.1,2.5,3.0,1.1,'Iris-versicolor'],
-        [5.7,2.8,4.1,1.3,'Iris-versicolor'],
-        [6.3,3.3,6.0,2.5,'Iris-virginica'],
-        [5.8,2.7,5.1,1.9,'Iris-virginica'],
-        [7.1,3.0,5.9,2.1,'Iris-virginica'],
-        [6.3,2.9,5.6,1.8,'Iris-virginica'],
-        [6.5,3.0,5.8,2.2,'Iris-virginica']
+        [5.1, 3.5, 1.4, 0.2, 'Iris-setosa'],
+        [4.9, 3.0, 1.4, 0.2, 'Iris-setosa'],
+        [4.7, 3.2, 1.3, 0.2, 'Iris-setosa'],
+        [4.6, 3.1, 1.5, 0.2, 'Iris-setosa'],
+        [5.0, 3.3, 1.4, 0.2, 'Iris-setosa'],
+        [6.0, 2.2, 4.0, 1.0, 'Iris-versicolor'],
+        # [6.1, 2.9, 4.7, 1.4, 'Iris-versicolor'],
+        [5.4, 3.0, 4.5, 1.5, 'Iris-versicolor'],
+        [6.0, 3.4, 4.5, 1.6, 'Iris-versicolor'],
+        [6.7, 3.1, 4.7, 1.5, 'Iris-versicolor'],
+        [5.1, 2.5, 3.0, 1.1, 'Iris-versicolor'],
+        [5.7, 2.8, 4.1, 1.3, 'Iris-versicolor'],
+        [6.3, 3.3, 6.0, 2.5, 'Iris-virginica'],
+        [5.8, 2.7, 5.1, 1.9, 'Iris-virginica'],
+        [7.1, 3.0, 5.9, 2.1, 'Iris-virginica'],
+        [6.3, 2.9, 5.6, 1.8, 'Iris-virginica'],
+        [6.5, 3.0, 5.8, 2.2, 'Iris-virginica']
     ]
     test_data = {
-        (5.6,2.7,4.2,1.3): 'Iris-versicolor',
-        (5.7,3.0,4.2,1.2): 'Iris-versicolor',
-        (5.7,2.9,4.2,1.3): 'Iris-versicolor',
+        (5.6, 2.7, 4.2, 1.3): 'Iris-versicolor',
+        (5.7, 3.0, 4.2, 1.2): 'Iris-versicolor',
+        (5.7, 2.9, 4.2, 1.3): 'Iris-versicolor',
     }
     results = {}
     for item, cl in test_data.items():
         spread, q = generate_queue(item, dataset, 8)
         # results[item] = 100 if  
-        print(item, spread, q)
+        # print(item, spread, q)
+        print(spread)
 
 if __name__ == '__main__':
     main()
