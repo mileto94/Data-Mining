e74db74d893b85e9c2e0b6bb77177c7a59a5e05a
Milka Ferezliyska <mileto94@abv.bg>
2016-12-25 00:53:30 +0200

    Add more depth of the tree on each iteration. TODO: Fix problem with creating inner level of the tree.

--- homework8/id3.py
+++ homework8/id3.py
@@ -28,7 +28,9 @@ def sort_by_attr(data, attr_id, class_id):
 
 
 def main():
-    attributes = ['gender', 'car ownership', 'travel cost', 'income level']  # , 'transportation'
+    attributes = ['gender', 'car ownership', 'travel cost', 'income level', 'transportation']
+    all_attributes = attributes[::]
+
     data = [
         ('male', 0, 'cheap', 'low', 'bus'),
         ('male', 1, 'cheap', 'medium', 'bus'),
@@ -48,52 +50,62 @@ def main():
         ('female', 1, 'cheap', 'high', '')  # Cherry
     ]
 
-    class_id = -1
-    all_gains = {}
     tree = {}
+    class_id = -1
+    current_id = -1
+    single = None
+    current = {}
+    while len(attributes):
+        attributes.pop(current_id)
+        all_gains = {}
 
-    # find probability for class
-    calculated = get_attr_probabilities(data, class_id)
-    # calculate entropy for class
-    print(calculated)
-    entropy = calculate_entropy(calculated)
-    print('entropy', entropy)
+        # find probability for class
+        calculated = get_attr_probabilities(data, class_id)
+        # calculate entropy for class
+        print(calculated)
+        entropy = calculate_entropy(calculated)
+        print('entropy', entropy)
 
-    for attr_id, attr in enumerate(attributes):
-        print(attr_id, attr)
+        for attr in attributes:
+            attr_id = all_attributes.index(attr)
+            print(attr_id, attr)
 
-        sorted_by_attr = sort_by_attr(data, attr_id, class_id)
-        probabilities = get_attr_probabilities(data, attr_id)
-        entropies = {}
-        for key, values in sorted_by_attr.items():
-            print(key, values)
-            sorted_values = get_attr_probabilities(values, 1)
-            print(key, sorted_values)
-            entropies[key] = calculate_entropy(sorted_values)
-        print(entropies)
-        # calculate gain
-        gain = entropy
-        for k, v in entropies.items():
-            gain -= probabilities[k] * v
-        print('Gain: ', gain)
-        all_gains[attr] = gain
-        print('----------------------------------------------------')
-    print("All gains", all_gains)
-    maximum_gain = max(all_gains.values())
-    root = [(k, v) for k, v in all_gains.items() if v == maximum_gain][0]
-    print(root)
-    root_id = attributes.index(root[0])
-    sorted_by_root = sort_by_attr(data, root_id, class_id)
-    tree[root[0]] = {}
-    for key, val in sorted_by_root.items():
-        item_value = [i[1] for i in val]
-        if len(set(item_value)) == 1:
-            tree[root[0]][key] = item_value[0]
-        else:
-            class_id = root_id
-            tree[root[0]][key] = {}
-            print('CHANGE class_id', class_id)
-    print(tree)
+            sorted_by_attr = sort_by_attr(data, attr_id, class_id)
+            probabilities = get_attr_probabilities(data, attr_id)
+            entropies = {}
+            for key, values in sorted_by_attr.items():
+                print(key, values)
+                sorted_values = get_attr_probabilities(values, 1)
+                print(key, sorted_values)
+                entropies[key] = calculate_entropy(sorted_values)
+            print(entropies)
+            # calculate gain
+            gain = entropy
+            for k, v in entropies.items():
+                gain -= probabilities[k] * v
+            print('Gain: ', gain)
+            all_gains[attr] = gain
+            print('----------------------------------------------------')
+        print("All gains", all_gains)
+        maximum_gain = max(all_gains.values())
+        root = [(k, v) for k, v in all_gains.items() if v == maximum_gain][0]
+        print(root)
+        root_id = all_attributes.index(root[0])
+        sorted_by_root = sort_by_attr(data, root_id, class_id)
+        if not tree:
+            tree[root[0]] = {}
+        for key, val in sorted_by_root.items():
+            item_value = [i[1] for i in val]
+            if len(set(item_value)) == 1:
+                tree[root[0]][key] = item_value[0]
+            else:
+                current_id = root_id
+                tree[root[0]][key] = current
+                single = key
+                print('CHANGE class_id', current_id)
+        print(tree)
+        data = [item for item in data if single in item]
+    print('FINALLY', tree)
 
 if __name__ == '__main__':
     main()
