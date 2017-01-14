647954b1c43999ad168a47aeb4f5718557636fda
Milka Ferezliyska <mileto94@abv.bg>
2017-01-14 15:01:56 +0200

    Prepare homework5

--- homework5/tic_tac_toe.py
+++ homework5/tic_tac_toe.py
@@ -1,116 +1,217 @@
-"""
-Pseudo code:
-
-function minimax(node, depth, isMaximizingPlayer, alpha, beta):
-    if node is a leaf node: // i.e. depth == 3
-        return value of the node
-
-    if isMaximizingPlayer:
-        bestVal = -INFINITY
-        for each child node:
-            value = minimax(node, depth + 1, false, alpha, beta)
-            bestVal = max(bestVal, value)
-            alpha = max(alpha, bestVal)
-            if beta <= alpha:
-                break
-        return bestVal
-
-    else:
-        bestVal = +INFINITY
-        for each child node:
-            value = minimax(node, depth + 1, true, alpha, beta)
-            bestVal = min(bestVal, value)
-            beta = min(beta, bestVal)
-            if beta <= alpha:
-                break
-        return bestVal
-"""
-
-from copy import deepcopy
-
-INFINITY = 1000000
+import json
+from random import randint
+from collections import OrderedDict
+from heapq import heappush, nlargest
 
 
-def generate_children(node):
-    result = []
-    for i, row in enumerate(node):
-        if '_' in row:
-            for idx in range(3):
-                if row[idx] == '_':
-                    child = deepcopy(node)
-                    child[idx][i] = 'X'
-                    result.append(child)
-    return result
+COMP_SIGN = 'O'
+PLAYER_SIGN = 'X'
+INFINITY = 100
 
 
-def minimax(node, depth, is_max_player, alpha, beta):
-    mark = 'X'
-    if depth == 3:
-        # return 1
-        if (mark == node[0][0] == node[0][1] == node[0][2] or  # row 0
-        mark == node[1][0] == node[1][1] == node[1][2] or  # row 1
-        mark == node[2][0] == node[2][1] == node[2][2] or  # row 2
-        mark == node[0][0] == node[1][0] == node[2][0] or  # column 0
-        mark == node[0][1] == node[1][1] == node[2][1] or  # column 1
-        mark == node[0][2] == node[1][2] == node[2][2] or  # column 2
-        mark == node[0][0] == node[1][1] == node[2][2] or  # diagonal
-        mark == node[0][2] == node[1][1] == node[2][0]):  # rev diag
-            return 1
-        elif ('O' == node[0][0] == node[0][1] == node[0][2] or  # row 0
-                              'O' == node[1][0] == node[1][1] == node[1][2] or  # row 1
-                              'O' == node[2][0] == node[2][1] == node[2][2] or  # row 2
-                              'O' == node[0][0] == node[1][0] == node[2][0] or  # column 0
-                              'O' == node[0][1] == node[1][1] == node[2][1] or  # column 1
-                              'O' == node[0][2] == node[1][2] == node[2][2] or  # column 2
-                              'O' == node[0][0] == node[1][1] == node[2][2] or  # diagonal
-                              'O' == node[0][2] == node[1][1] == node[2][0]):
-            return -1
+class TicTacToe:
+    def __init__(self, fields=[]):
+        self.size = 3
+        self.empty = '_'
+        self.fields = fields if fields else OrderedDict(
+            {(i, j): self.empty for j in range(self.size) for i in range(self.size)})
+
+        self.winning_states = [[(i, j) for j in range(self.size)] for i in range(self.size)]  # rows
+        self.winning_states.extend([[(i, j) for i in range(self.size)] for j in range(self.size)])  # cols
+        self.winning_states.append([(i, i) for i in range(self.size)])
+        self.winning_states.append([(0, 2), (1, 1), (2, 0)])
+
+    def __str__(self):
+        board = []
+        for i in range(self.size):
+            board.append('|'.join([self.fields[i, j] for j in range(self.size)]))
+        return '\n'.join(board)
+
+    def __repr__(self):
+        return str(self.fields)
+
+    def get_enemy(self, sign):
+        return PLAYER_SIGN if sign == COMP_SIGN else COMP_SIGN
+
+    def get_empty_fields(self):
+        return [pos for pos, val in self.fields.items() if val == self.empty]
+
+    def get_played_fields(self, player):
+        return [field for field, pl in self.fields.items() if pl == player]
+
+    def wins(self):
+        played = {
+            COMP_SIGN: self.get_played_fields(COMP_SIGN),
+            PLAYER_SIGN: self.get_played_fields(PLAYER_SIGN)
+        }
+        for sign, positions in played.items():
+            if positions:
+                for combo in self.winning_states:
+                    played = [True for win in combo if win in positions]
+                    if len(played) == self.size:
+                        return sign
+        return None
+
+    def is_over(self):
+        if len(self.get_empty_fields()) == 0 or self.wins() is not None:
+            return True
+        return False
+
+    def get_score(self):
+        if self.wins() == COMP_SIGN:
+            return 10
+        elif self.wins() == PLAYER_SIGN:
+            return -10
+        return 0
+
+    def user_play(self):
+        position = input('Enter position with space: ')
+        values = list(map(int, position.split(' ')))
+        if len(values) < 2:
+            print('This filed is not empty or the coordinates are not valid!')
+            return self.user_play()
+        x, y = values
+        if -1 < x < 3 and -1 < y < 3 and self.fields[x, y] == self.empty:
+            self.fields[x, y] = PLAYER_SIGN
         else:
-            return 0
+            print('This filed is not empty or the coordinates are not valid!')
+            return self.user_play()
 
-    if is_max_player:
-        # print('MAX turn')
+    def computer_play(self):
+        # board, val = minimax(self, COMP_SIGN, -INFINITY, INFINITY)
+        # print('Result from minimax:', val)
+        # self.fields = board.fields.copy()
+        scores = []
+        for pos in self.get_empty_fields():
+            b1 = TicTacToe(fields=self.fields.copy())
+            b1.move(pos, COMP_SIGN)
+            v, d = minimax(b1, PLAYER_SIGN, -INFINITY, INFINITY)
+            # print('******************************************************')
+            # print(v, 'at depth', d)
+            # print('******************************************************')
+            heappush(scores, (v, pos))
+        # print(scores)
+        sc, best_position = nlargest(1, scores)[0]
+        # print('BEST move is', best_position, 'with score', sc)
+        print('Computer plays on {}...'.format(best_position))
+        self.move(best_position, COMP_SIGN)
+
+    def play(self, is_max):
+        print(self)
+        print()
+
+        while True:
+            # print(self.get_empty_fields())
+
+            if self.is_over():
+                return self.wins()
+            elif is_max:
+                self.computer_play()
+                is_max = False
+            else:
+                self.user_play()
+                is_max = True
+            print(self)
+            print()
+
+    def move(self, position, sign):
+        if tuple(position) in self.fields.keys() and position in self.get_empty_fields():
+            self.fields[position] = sign
+
+
+def minimax(board, player, alpha, beta, depth=0):
+    if board.is_over():
+        return board.get_score(), depth
+
+    if player == COMP_SIGN:
         best_value = -INFINITY
-        for child in generate_children(node):
-            # print_board(child)
-            value = minimax(node, depth + 1, False, alpha, beta)
-            best_value = max(best_value, value)
-            alpha = max(alpha, best_value)
-            if beta <= alpha:
+        for pos in board.get_empty_fields():
+            # child is the same board with 1 additional move on one of the free places
+            child = TicTacToe(fields=board.fields.copy())
+            child.move(pos, player)
+            # print(child)
+            current_value, d = minimax(child, board.get_enemy(player), alpha, beta, depth=depth + 1)
+            best_value = max(current_value, best_value)
+            alpha = max(best_value, alpha)
+            if alpha >= beta:
+                # beta prunning
                 break
-        return best_value
+        return best_value, depth
+
     else:
-        # print('min turn')
         best_value = INFINITY
-        for child in generate_children(node):
-            # print_board(child)
-            value = minimax(node, depth + 1, True, alpha, beta)
-            best_value = min(best_value, value)
-            beta = min(beta, best_value)
+        for pos in board.get_empty_fields():
+            # child is the same board with 1 additional move on one of the free places
+            child = TicTacToe(fields=board.fields.copy())
+            child.move(pos, player)
+            # print(child)
+            current_value, d = minimax(child, board.get_enemy(player), alpha, beta, depth=depth + 1)
+            best_value = min(current_value, best_value)
+            beta = min(current_value, beta)
             if beta <= alpha:
+                # alpha prunning
                 break
-        return best_value
+        return best_value, depth
 
-
-def print_board(node):
-    rows = ['|'.join(node[r]) for r in range(3)]
-    print('\n'.join(rows))
-    print()
+    # best_value = -INFINITY if player == COMP_SIGN else INFINITY
+    # for pos in board.get_empty_fields():
+    #     # child is the same board with 1 additional move on one of the free places
+    #     # child = TicTacToe(fields=board.fields.copy())
+    #     board.move(pos, player)
+    #     # print(child)
+    #     current_value = minimax(child, board.get_enemy(player), alpha, beta, depth=depth + 1)
+    #     if player == COMP_SIGN:
+    #         best_value = max(current_value, best_value)
+    #         if current_value >= beta:
+    #             # beta prunning
+    #             break
+    #         alpha = current_value
+    #     else:
+    #         best_value = min(current_value, best_value)
+    #         if current_value <= alpha:
+    #             # alpha prunning
+    #             break
+    #         beta = current_value
+    # return best_value - depth
 
 
 def main():
-    # Calling the function for the first time.
-    node = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
-    print_board(node)
-    res = minimax(node, 0, True, -INFINITY, INFINITY)
-    # TODO: If there is still place in the board, make a move.
-    if res == 0:
-        print('0 : 0')
-    elif res == 1:
-        print('X wins!')
+    # tic_tac_toe()
+    rand_num = randint(0, 1000)
+    print('Fill in board with 0-based numbers!!!')
+    if rand_num % 2 == 0:
+        # computer
+        print('The first player is computer. He plays with O. You play with X.')
+        is_computer = True
     else:
-        print('O wins!')
-    print_board(node)
+        # player
+        print('Yey, the first player is you! The computer plays with O. You play with X.')
+        is_computer = False
+
+    board = TicTacToe()
+
+    # # test fixes
+    # board.fields.update({
+    #     (0, 1): 'X',
+    #     (1, 2): 'X',
+    #     (2, 0): 'O',
+    #     (2, 1): 'O',
+    #     (2, 2): 'X'
+    # })
+    # global COMP_SIGN, PLAYER_SIGN
+    # COMP_SIGN = 'X'
+    # PLAYER_SIGN = 'O'
+    # is_computer = False
+
+    winner = board.play(is_computer)
+    winners = {
+        COMP_SIGN: 'computer...',
+        PLAYER_SIGN: 'you!',
+        None: 'no one :)'
+    }
+    print('The winner is {}'.format(winners[winner]))
+    print(board)
+
 
 if __name__ == '__main__':
     main()
