1459fc1e7785fea22a654336cb9e3644b6d3d85e
Milka Ferezliyska <mileto94@abv.bg>
2017-01-20 01:56:59 +0200

    Add comments in code

--- homework8/new_id3.py
+++ homework8/new_id3.py
@@ -47,6 +47,8 @@ def build_tree(old_attributes, data):
     attributes_order = []
     root_id = None
     while len(attributes):
+
+        # if there is an error with the indexes always get the last one
         if current_id >= len(attributes):
             current_id = -1
         attributes.pop(current_id)
@@ -67,11 +69,12 @@ def build_tree(old_attributes, data):
             probabilities = get_attr_probabilities(data, attr_id)
             entropies = {}
             for key, values in sorted_by_attr.items():
+                # calculate entropies for each attribute
                 sorted_values = get_attr_probabilities(values, 1)
                 entropies[key] = calculate_entropy(sorted_values)
             # print(entropies)
 
-            # calculate gain
+            # calculate gain for each attribute
             gain = entropy
             for k, v in entropies.items():
                 gain -= probabilities[k] * v
@@ -79,20 +82,26 @@ def build_tree(old_attributes, data):
             all_gains[attr] = gain
         # print("All gains", all_gains)
         if all_gains:
+            # get the attribute with max gain fot subroot of the tree
             maximum_gain = max(all_gains.values())
             root = [(k, v) for k, v in all_gains.items() if v == maximum_gain][0]
         new_root_id = all_attributes.index(root[0])
 
         if root_id is not None:
+            # update previous {} values to relate the new key(subroot)
             for k, v in tree[all_attributes[root_id]].items():
                 if len(v) == 0:
                     tree[all_attributes[root_id]][k] = all_attributes[new_root_id]
         root_id = new_root_id
+
         if root_id not in attributes_order:
+            # update ordered list of attributes
             attributes_order.append(root_id)
+
         sorted_by_root = sort_by_attr(data, root_id, class_id)
         tree[root[0]] = {}
         for key, val in sorted_by_root.items():
+            # add new subroot to the tree and fill in its values
             item_value = [i[1] for i in val]
             if len(set(item_value)) == 1:
                 tree[root[0]][key] = item_value[0]
@@ -100,7 +109,7 @@ def build_tree(old_attributes, data):
                 tree[root[0]][key] = {}
                 current_id = root_id
                 single = key
-                # print('CHANGE class_id', current_id)
+        # continue filtering only filterd by current subroot instances (not the whole data)
         data = [item for item in data if single in item]
     # print('FINALLY', tree)
     return tree, attributes_order
@@ -116,23 +125,22 @@ def get_test_data(data, k=20):
 
 def get_results(tree, test_data, attributes, attributes_order, end_values):
     res = {}
+    count = 0
     for test in test_data:
         current_node = None
         for attr in attributes_order:
             val = attributes[attr]
-            # print('Looking for "{}"'.format(val))
             if tree.get(val, None) in attributes:
-                # print('GO Inside')
                 current_node = tree.get(attributes[attr], {})
             else:
-                # print(tree.get(attributes[attr], {}))
                 current_node = tree.get(attributes[attr], {}).get(test[attr], {})
-                # print(current_node)
             if current_node and current_node in end_values:
-                # print('Found!!!', current_node)
                 res[tuple(test)] = (current_node, test[-1], test[-1] == current_node)
                 break
-    return res
+        count = count + 1 if test[-1] == current_node else count
+    accuracy = 100 * count / len(test_data)
+    print('Accuracy: {}%'.format(accuracy))
+    return res, accuracy
 
 
 def main():
@@ -168,21 +176,29 @@ def main():
 
 
 def main1():
-    attributes = read_data('attributes.csv')[0]
-    data = read_data('data.csv')
+    count = 0
+    res = []
+    while count < 3:
+        attributes = read_data('attributes.csv')[0]
+        data = read_data('data.csv')
 
-    trainers, test_data = get_test_data(data)
-    print('test data:')
-    print(test_data)
-    print()
+        trainers, test_data = get_test_data(data, k=70)
+        # print('test data:')
+        # print(test_data)
+        # print()
 
-    end_values = set([i[len(i) - 1] for i in data])
-    print('End values:', end_values)
-    print()
+        end_values = set([i[len(i) - 1] for i in data])
+        # print('End values:', end_values)
+        # print()
 
-    tree, attributes_order = build_tree(attributes, data)
-    # get result
-    pprint.pprint(get_results(tree, test_data, attributes, attributes_order, end_values))
+        tree, attributes_order = build_tree(attributes, data)
+        # get result
+        results, accuracy = get_results(tree, test_data, attributes, attributes_order, end_values)
+        # pprint.pprint(results)
+        res.append(accuracy)
+        count += 1
+    print(count)
+    print(sum(res) / (count))
 
 
 if __name__ == '__main__':
